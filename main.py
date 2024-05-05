16b4281dd98e013fc8afd910574dd47a17d8a913
Marina Galchenkova <108687313+galchenm@users.noreply.github.com>
2024-05-05 15:25:44 +0200

    change match case to if else statements

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 from typing import List
 
 user_prompt = "Type add, show, edit, complete or exit: "
-user_edit = "Type the number of to-do item you want to edit: "
 script_dir = os.path.dirname(__file__)
 to_dos_file = os.path.join(script_dir, "to_dos.txt")
 to_dos: List[str]
@@ -20,62 +19,63 @@ else:
 
 while True:
     user_action = input(user_prompt).strip().lower()
-    match user_action:
-        case "add":
-            user_todo = input("Enter a to-do: ")
-            to_dos.append(user_todo)
-            with open(to_dos_file, "a") as file:   
-                file.write(user_todo + "\n")
-        case "show":
-            if not to_dos:
-                print("You have no to-dos.")
-            else:
-                for index, item in enumerate(to_dos):
-                    item = item.capitalize()
-                    row = f'{index + 1} - {item}'
-                    print(row)
-        case "edit":
+    
+    if "add" in user_action:
+        user_todo = user_action[4:]
+        to_dos.append(user_todo)
+        with open(to_dos_file, "a") as file:   
+            file.write(user_todo + "\n")
+    elif "show" in user_action:
+        if not to_dos:
+            print("You have no to-dos.")
+        else:
+            for index, item in enumerate(to_dos):
+                item = item.capitalize()
+                row = f'{index + 1} - {item}'
+                print(row)
+    elif "edit" in user_action:
+        if not to_dos:
+            print("You have no to-dos.") 
+        else: 
+            try:             
+                to_edit = int(user_action[5:])
+                if to_edit > len(to_dos):
+                    print("You are out of range.")
+                else:
+                    new_todo = input("Enter a new to-do: ")
+                    to_dos[to_edit - 1] = new_todo
+                    with open(to_dos_file, "w") as file:
+                        for item in to_dos:
+                            file.write(item + "\n")
+            except ValueError:
+                print("Invalid input")
+    elif "complete" in user_action:
             if not to_dos:
                 print("You have no to-dos.") 
-            else: 
-                try:             
-                    to_edit = int(input(user_edit))
-                    if to_edit > len(to_dos):
-                        print("You are out of range.")
-                    else:
-                        new_todo = input("Enter a new to-do: ")
-                        to_dos[to_edit - 1] = new_todo
+            else:               
+                to_complete = int(user_action[9:])
+                if to_complete > len(to_dos):
+                    print("You are out of range.")
+                else:
+                    try:
+                        index = to_complete - 1
+                        removed_task = to_dos[index] 
+                        print(f"{removed_task} has been removed.")
+                        to_dos.pop(index)
                         with open(to_dos_file, "w") as file:
                             for item in to_dos:
                                 file.write(item + "\n")
-                except ValueError:
-                    print("Invalid input")
-        case "complete":
-                if not to_dos:
-                    print("You have no to-dos.") 
-                else:   
-                    to_complete = int(input("Enter the number of to-do item you want to complete: "))            
-                    if to_complete > len(to_dos):
-                        print("You are out of range.")
-                    else:
-                        try:
-                            index = to_complete - 1
-                            removed_task = to_dos[index] 
-                            to_dos.pop(index)
-                            with open(to_dos_file, "w") as file:
-                                for item in to_dos:
-                                    file.write(item + "\n")
-                            message = "You have no to-dos." if not to_dos else f"{removed_task} has been removed."
-                            print(message)
-                        except ValueError:
-                            print("Invalid input")
-        case "exit":
-            with open(to_dos_file, "w") as file:
-                for item in to_dos:
-                    file.write(item + "\n")
-            break
-        case whatever:
-            print("Invalid input")
+                        message = "You have no to-dos." if not to_dos else f"{removed_task} has been removed."
+                        print(message)
+                    except ValueError:
+                        print("Invalid input")
+    elif "exit" in user_action:
+        with open(to_dos_file, "w") as file:
+            for item in to_dos:
+                file.write(item + "\n")
+        break
+    else:
+        print("Invalid input")
     
 
 print(f"You entered: {to_dos}")
