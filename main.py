6121db873fbbb3ca3aa34ee0db487f27ba6e0587
Marina Galchenkova <108687313+galchenm@users.noreply.github.com>
2024-05-10 09:01:48 +0200

    fixing bug with handling user prompts

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ else:
 while True:
     user_action = input(user_prompt).strip().lower()
     
-    if "add" in user_action or 'new' in user_action or 'more' in user_action:
+    if user_action.startswith("add") or user_action.startswith("new") or user_action.startswith("more"):
         user_todo = user_action[4:]
         to_dos.append(user_todo)
         with open(to_dos_file, "a") as file:   
             file.write(user_todo + "\n")
-    elif "show" in user_action:
+    elif user_action.startswith("show") or user_action.startswith("display"):
         if not to_dos:
             print("You have no to-dos.")
         else:
@@ -33,7 +33,7 @@ while True:
                 item = item.capitalize()
                 row = f'{index + 1} - {item}'
                 print(row)
-    elif "edit" in user_action:
+    elif user_action.startswith("edit") or user_action.startswith("change") or user_action.startswith("modify"):
         if not to_dos:
             print("You have no to-dos.") 
         else: 
@@ -48,8 +48,8 @@ while True:
                         for item in to_dos:
                             file.write(item + "\n")
             except ValueError:
-                print("Invalid input")
-    elif "complete" in user_action:
+                print("Invalid input. Please enter a number.")
+    elif user_action.startswith("complete") or user_action.startswith("done") or user_action.startswith("finish")  or user_action.startswith("remove"):
             if not to_dos:
                 print("You have no to-dos.") 
             else:               
@@ -68,8 +68,8 @@ while True:
                         message = "You have no to-dos." if not to_dos else f"{removed_task} has been removed."
                         print(message)
                     except ValueError:
-                        print("Invalid input")
-    elif "exit" in user_action:
+                        print("Invalid input. Please enter a number.")
+    elif user_action.startswith("exit") or user_action.startswith("quit") or user_action.startswith("stop") or user_action.startswith("end"):
         with open(to_dos_file, "w") as file:
             for item in to_dos:
                 file.write(item + "\n")
