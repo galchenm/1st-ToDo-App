711265f81daea52e92cf4f45286b5f66574deec8
Marina Galchenkova <108687313+galchenm@users.noreply.github.com>
2024-05-28 20:37:20 +0300

    fixed bugs and added images for two buttons

--- gui.py
+++ gui.py
@@ -5,18 +5,19 @@ import time
 sg.theme('DarkAmber')
 
 clock = sg.Text('',key="clock")
+
 add_label = sg.Text("Type in a to-do")
 add_input_box = sg.InputText(tooltip="Enter to-do", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button(size=10, image_source="add.png", mouseover_colors="LightBlue2", tooltip="Add to-do", key="Add")
 
 list_box = sg.Listbox(values=functions.get_todos(), key='todos', 
                       enable_events=True, size=[45,10])
 edit_button = sg.Button("Edit")
 
-complete_button = sg.Button("Completed")
+complete_button = sg.Button(size=40, image_source="complete.png", mouseover_colors="LightBlue2", tooltip="Select to-do to complete", key="Completed")
 exit_button = sg.Button("Exit")
 
-layout = [[clock],[add_label], 
+layout = [[clock], [add_label], 
             [add_input_box, add_button], 
             [list_box, edit_button, complete_button],
             [exit_button]]
@@ -25,15 +26,23 @@ window = sg.Window('My To-Do App',
                    layout=layout, 
                    font=('Helvetica', 16))
 while True:
-    event, values = window.read(timeout=200)
-    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+    event, values = window.read(timeout=100)
+    if event in (sg.WINDOW_CLOSED, None):
+        break  # exit loop
+    else:
+        window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "Add":
-            to_dos = functions.get_todos()
-            user_todo = values['todo']
-            to_dos.append(user_todo)
-            functions.write_todos(to_dos)
-            window['todos'].update(values=to_dos)
+            try:
+                to_dos = functions.get_todos()
+                if not values['todo']:
+                    raise IndexError
+                user_todo = values['todo']
+                to_dos.append(user_todo)
+                functions.write_todos(to_dos)
+                window['todos'].update(values=to_dos)
+            except IndexError:
+                sg.popup("Please enter a to-do.", font=('Helvetica', 16))
         case "Edit":
             try:
                 todo_to_edit = values['todos'][0]
