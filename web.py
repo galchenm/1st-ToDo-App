09cdf3cea88c8707d34b2bc692b7cfb6dce67f67
Marina Galchenkova <108687313+galchenm@users.noreply.github.com>
2024-06-02 15:26:28 +0300

    clean the code in web version

--- web.py
+++ web.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from modules import functions
 
+
 todos = functions.get_todos()
+
 
 def add_todo():
     new_todo = st.session_state["new_todo"]
@@ -9,16 +11,21 @@ def add_todo():
     functions.write_todos(todos)
 
 
+
 st.title("My To-do App")
+
 st.subheader("Developed by M. Galchenkova")
+
 st.write("This app is to increase your productivity.")
 
+
 for index, todo in enumerate(todos):
-    chechbox = st.checkbox(todo.title(), key=todo)
-    if chechbox:
+    checkbox = st.checkbox(todo.title(), key=todo)
+    if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
         del st.session_state[todo]
         st.experimental_rerun()
 
 st.text_input(label="Enter a new to-do", placeholder="Add a new to-do", label_visibility="hidden", on_change=add_todo, key="new_todo")
+
